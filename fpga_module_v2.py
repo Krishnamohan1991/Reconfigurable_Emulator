02ce517744d6eb09d8978a2410278e2bc9d48d5b
Krishna91 <krish.pathi91@gmail.com>
2017-02-18 23:03:25 -0500

    file prior to some cleaning up.grammar added for switch block configuration

--- fpga_module_v2.py
+++ fpga_module_v2.py
@@ -1,89 +1,108 @@
 from pyparsing import*
-
+from modules import*
+from fpga_dict import*
 #ParserElement.setDefaultWhitespaceChars(' \t')
+
+
 
 i_consider_whitespaces_to_be_only = ' '
 ParserElement.setDefaultWhitespaceChars(i_consider_whitespaces_to_be_only)
 
-
-LUT_connect={"Q1":['00','04'],"Q2":['00','04'],"Q3":['00','01'],"Q4":['00','01'],"Q5":['00','02'],"Q6":['00','02'],"Q7":['00','00'],"Q8":['00','00'],
-
-	     "Q9":['01','14'],"Q10":['01','14'],"Q11":['01','11'],"Q12":['01','11'],"Q13":['01','12'],"Q14":['01','12'],"Q15":['01','10'],"Q16":['01','10'],
-		
-             "Q17":['02','24'],"Q18":['02','24'],"Q19":['02','21'],"Q20":['02','21'],"Q21":['02','22'],"Q22":['02','22'],"Q23":['02','20'],"Q24":['02','20'],   
-            
-             "Q25":['03','34'],"Q26":['03','34'],"Q27":['03','31'],"Q28":['03','31'],"Q29":['03','32'],"Q30":['03','32'],"Q31":['03','30'],"Q32":['03','30']
-	    }
-
-
-
-#cb_connect is the dictionary whose key field store the Connection block ID and values: i)CB code(identical for CB sharing the same global lines) 
-#  ii)SB on the left iii)SB on the right
-
-CB_connect={"00":['C1','00','01'],"01":['C2','01','11'],"02":['C3','10','11'],"03":['C4','00','10'],
-
-	    "10":['C5','01','02'],"11":['C6','02','12'],"12":['C7','11','12'],"13":['C2','01','11'],
-            
-	    "20":['C7','11','12'],"21":['C8','12','22'],"22":['C9','21','22'],"23":['C10','11','21'],
-
-	    "30":['C3','10','11'],"31":['C10','11','21'],"32":['C11','20','21'],"33":['C12','10','20']
-	   }
-
-LUT_function={"&":'1000000000000000',"|":'0111111111111111',"^":'0110100110010110'}
-
-
-LUT_interconnect={"I0":'00000',"I1":'00001',"I2":'00010',"I3":'00011',"I4":'00100',
-"I5":'00101',"I6":'00110',"I7":'00111',"I8":'01000',"I9":'01001',"I10":'01010',"I11":'01011',"I12":'01100',
-"I13":'01101',"I14":'01110',"I15":'01111',"Q1":'10000',"Q2":'10001',"Q3":'10010',"Q4":'10011',"Q5":'10100',
-"Q6":'10101',"Q7":'10110',"Q8":'10111',"Q9":'10000',"Q10":'10001',"Q11":'10010',"Q12":'10011', "Q13":'10100',
-"Q14":'10101',"Q15":'10110',"Q16":'10111',"Q17":'10000',"Q18":'10001',"Q19":'10010',"Q20":'10011', "Q21":'10100',
-"Q22":'10101', "Q23":'10110',"Q24":'10111', "Q25":'10000',"Q26":'10001', "Q27":'10010',"Q28":'10011', "Q29":'10100',
-"Q30":'10101', "Q31":'10110',"Q32":'10111'}
-
-
+def configureLUT(lutid,lutfunc,inp1,inp2,inp3,inp4,mux):
+	s=str(lutid)
+	#print lutobjectDictionary[s]
+	
+	lut.lutConfig(lutobjectDictionary[s],s,lutfunc,inp1,inp2,inp3,inp4,mux)
+	#print lut.bits(lutobjectDictionary[s])
+	
+	
+	#print Q1.lutTable
+	#print Q2.lutTable
+	#print lutObject
+	#lutObject.lutTable=lutfunc
+	#print lutObject.lutTable
+	#lutObject.muxSwitch=mux
+	#print lutObject.muxSwitch
+	#lutObject.outputPort=lutid
+	#print lutObject.outputPort
+	#lutObject.inputPort1=inp1
+	#lutObject.inputPort2=inp2
+	#lutObject.inputPort3=inp3
+	#lutObject.inputPort4=inp4
+	
 def begins(cls):
-	#c=str(cls[2][0])
-	print LUT_function[cls.function],
-	print LUT_interconnect[cls.LUTID],
-	print LUT_interconnect[cls.op1],
-	print LUT_interconnect[cls.op2],
-	print LUT_interconnect[cls.op3],
-	print LUT_interconnect[cls.op4]
-	#print cls.LUTId
-	#print LUT_interconnect[c]
 
-input_port = oneOf("I0 I1 I2 I3 I4 I5 I6 I7 I8 I9 I10 I11 I12 I13 I14 I15").setResultsName('InputLine')
-output_port = oneOf("Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8").setResultsName('LUTID')
+	
+	if(LUT_connect.get(cls.LUTID,0)):
+		configureLUT(cls.LUTID,LUT_function[cls.function],LUT_interconnect[cls.op1],LUT_interconnect[cls.op2],LUT_interconnect[cls.op3],LUT_interconnect[cls.op4],
+		cls.MUXswitch)
+	
+	'''	
+
+	if (cls.LUTID=='Q1' or cls.LUTID=='Q2' or cls.LUTID=='Q3' or cls.LUTID=='Q4' or cls.LUTID=='Q5' or 
+	   cls.LUTID=='Q6' or cls.LUTID=='Q7' or cls.LUTID=='Q8' or cls.LUTID=='Q9' or cls.LUTID=='Q10' or cls.LUTID=='Q11'
+	   or cls.LUTID=='Q12' or cls.LUTID=='Q13' or cls.LUTID=='Q14' or cls.LUTID=='Q15' or cls.LUTID=='Q16' or cls.LUTID=='Q17' 
+	   or  cls.LUTID=='Q18' or cls.LUTID=='Q19' or cls.LUTID=='Q20' or cls.LUTID=='Q21' or cls.LUTID=='Q22' or cls.LUTID=='Q23' 
+	   or cls.LUTID=='Q24' or cls.LUTID=='Q25' or cls.LUTID=='Q26' or cls.LUTID=='Q27' or cls.LUTID=='Q28' or cls.LUTID=='Q29' 
+	   or  cls.LUTID=='Q30' or cls.LUTID=='Q31' or     cls.LUTID=='Q32') :
+		configureLUT(cls.LUTID,LUT_function[cls.function],LUT_interconnect[cls.op1],LUT_interconnect[cls.op2],LUT_interconnect[cls.op3],LUT_interconnect[cls.op4],
+		cls.MUXswitch)
+	'''
+			
+		
+	
+	return 1
+
+input_port = (oneOf("I0 I1 I2 I3 I4 I5 I6 I7 I8 I9 I10 I11 I12 I13 I14 I15 Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9 Q10 Q11 Q12 Q13 Q14 Q15 Q16 Q17 Q18 Q19 Q20 Q21 Q22 Q23 Q24 Q25 Q26 Q27 Q28 Q29 Q30 Q31 Q32")).setResultsName('InputLine')
+
+#inputLines=(oneOf("I0 I1 I2 I3 I4 I5 I6 I7 I8 I9 I10 I11 I12 I13 I14 I15")).setResultsName('onlyInput')
+output_port = (oneOf("Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9 Q10 Q11 Q12 Q13 Q14 Q15 Q16 Q17 Q18 Q19 Q20 Q21 Q22 Q23 Q24 Q25 Q26 Q27 Q28 Q29 Q30 Q31 Q32")).setResultsName('LUTID')
+#operand= input_port | output_port
+
 operand1 = (input_port | output_port).setResultsName('op1')
 operand2 = (input_port | output_port).setResultsName('op2')
 operand3 = (input_port | output_port).setResultsName('op3')
 operand4 = (input_port | output_port).setResultsName('op4')
-op = (oneOf("& | ^").setResultsName('function'))
+op = (oneOf("& | ^")).setResultsName('function')
+
+switchId = (oneOf("00 01 02 10 11 12 20 21 22")).setResultsName('switchID')
+fromFace=(oneOf("A B C D")).setResultsName('fromFace')
+toFace=(oneOf("A B C D")).setResultsName('toFace')
+faceIndex=(oneOf("0 1 2 3 4 5 6 7")).setResultsName('faceIndex')
+
 bopen=Literal("(").suppress()
 bclose=Literal(")").suppress()
 comma=Literal(",").suppress()
+sqopen=Literal("[").suppress()
+sqclose=Literal("]").suppress()
 equal=Literal("=").suppress()
+MUXswitch=(oneOf("1 0")).setResultsName('MUXswitch')
+config_code=(oneOf('IO' 'SB' 'CB')).setResultsName('configCode')
 
+#config_code=(oneOf('IO' 'SB' 'CB')).setResultsName('configCode')
 
 
 lut_rhs= operand1 + comma + operand2 + comma + operand3 + comma + operand4
 
-	
-expression = output_port + equal + op + bopen + lut_rhs + bclose 
-config=OneOrMore(expression)
+#lut_rhs= operand + comma + operand + comma + operand + comma + operand
 
- 
+	
+expression = output_port + equal + op + bopen + lut_rhs + bclose + sqopen + MUXswitch + sqclose  | \
+		config_code + switchId + bopen + fromFace + faceIndex + comma + toFace + faceIndex + bclose
+config=OneOrMore(expression)
 
 
 counter=0
 
 tests=open("data.txt","r")
 scr=tests.read()
+
 scr_split=scr.splitlines()
+#print scr_split
 
 #tests ="""\
-#Q1=&(I1,I2,I3,I4)
-#Q2=^(Q1,I3,I5,I6)
+#Q1=&(I1,I2,I3,I4)[1]
+#Q2=^(Q1,I3,I5,I6)[0]
 #""".splitlines()
 
 
@@ -92,9 +111,27 @@ scr_split=scr.splitlines()
 
 for test in scr_split:
 	stats = config.parseString(test)
+	#print stats
 	begins(stats)
+	
+#print stats.dump()
 #	counter+=1
 	
 tests.close()
-print counter
+
+print lutobjectDictionary['Q7'].status
+print lutobjectDictionary['Q1'].status
+
+print lutobjectDictionary['Q7'].outputPort
+print lutobjectDictionary['Q3'].bits() 
+print lutobjectDictionary['Q1'].bits()
+print lutobjectDictionary['Q7'].bits() 
+print lutobjectDictionary['Q5'].bits()
+    #print name
+
+#print Q1.bits()
+#print Q2.bits()
+#print Q2.lutTable
+#print Q3.lutTable
+#print counter
 	
